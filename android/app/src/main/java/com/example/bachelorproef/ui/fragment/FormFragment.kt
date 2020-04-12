bfd048726d3b7477e1cbe58e16efb288d9ad9d7b
navaronbracke <brackenavaron@gmail.com>
2020-04-12 23:47:46 +0200

    Added remaining form controls

--- android/app/src/main/java/com/example/bachelorproef/ui/fragment/FormFragment.kt
+++ android/app/src/main/java/com/example/bachelorproef/ui/fragment/FormFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatSpinner
 import androidx.fragment.app.Fragment
 import com.example.bachelorproef.R
 import com.example.bachelorproef.databinding.FragmentFormBinding
 import com.example.bachelorproef.viewmodel.FormViewModel
 import com.example.bachelorproef.viewmodel.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_form.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class FormFragment : Fragment() {
+class FormFragment : Fragment(), AdapterView.OnItemSelectedListener {
     private val sharedViewModel: SharedViewModel by sharedViewModel()
     private val viewModel: FormViewModel by viewModel()
 
@@ -27,5 +34,60 @@ class FormFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         (activity as AppCompatActivity).supportActionBar!!.title = getString(R.string.choice_forms)
+        setupSpinner()
+        setupProgressBar()
+    }
+
+    //SPINNER
+
+    /**
+     * This implementation of the adapter interface doesn't do anything specific
+     */
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        //do nothing
+    }
+
+    /**
+     * Notify the viewmodel
+     */
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        viewModel.onDropDownChanged(position)
+    }
+
+    /**
+     * Setup the [AppCompatSpinner] for the task priority
+     */
+    private fun setupSpinner()
+    {
+        val spinner = view?.findViewById<AppCompatSpinner>(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            context!!,
+            R.array.dropdown_items,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner?.adapter = adapter
+        }
+        spinner?.onItemSelectedListener = this
+        spinner?.setSelection(viewModel.getDropDownInput().value!!)
+    }
+
+    //PROGRESS BAR
+    private fun setupProgressBar(){
+        seekBar?.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                viewModel.onSliderChanged(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //do nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                //do nothing
+            }
+        })
     }
 }
--- android/app/src/main/java/com/example/bachelorproef/viewmodel/FormViewModel.kt
+++ android/app/src/main/java/com/example/bachelorproef/viewmodel/FormViewModel.kt
@@ -10,13 +10,27 @@ import com.example.bachelorproef.application.MyApplication
 import com.example.bachelorproef.util.IOnTextChangedListener
 
 class FormViewModel(application: Application) : AndroidViewModel(application) {
+    val dropdownValues = listOf(1,2,3,4,5)
     private val textInput = MutableLiveData("")
     private val textInputError = MutableLiveData<String>(null)
     val textInputDrawable = MutableLiveData<Drawable>(null)
     private val maxTextLength = 20
+    private val switchInput = MutableLiveData(false)
+    private val radio1Input = MutableLiveData(true)
+    private val radio2Input = MutableLiveData(false)
+    private val checkboxInput = MutableLiveData(false)
+    private val sliderInput = MutableLiveData(0)
+    private val dropdownInput = MutableLiveData(dropdownValues[0])
+    val sliderMax = 100
 
+    fun getSliderInput(): LiveData<Int> = sliderInput
+    fun getSwitchInput(): LiveData<Boolean> = switchInput
+    fun getRadio1Input(): LiveData<Boolean> = radio1Input
+    fun getRadio2Input(): LiveData<Boolean> = radio2Input
+    fun getCheckboxInput(): LiveData<Boolean> = checkboxInput
     fun getTextInput(): LiveData<String> = textInput
     fun getTextInputError(): LiveData<String> = textInputError
+    fun getDropDownInput(): LiveData<Int> = dropdownInput
 
     val onInputFieldOneChanged = object : IOnTextChangedListener {
         override fun onTextChanged(text: CharSequence?) {
@@ -33,5 +47,35 @@ class FormViewModel(application: Application) : AndroidViewModel(application) {
                 }
             }
         }
+    }
+
+    fun onSwitchChanged(){
+        switchInput.value = !switchInput.value!!
+    }
+
+    fun onRadio1Changed(){
+        if(!radio1Input.value!!){
+            radio1Input.value = true
+            radio2Input.value = false
+        }
+    }
+
+    fun onRadio2Changed(){
+        if(!radio2Input.value!!){
+            radio2Input.value = true
+            radio1Input.value = false
+        }
+    }
+
+    fun onCheckboxChanged(){
+        checkboxInput.value = !checkboxInput.value!!
+    }
+
+    fun onSliderChanged(newValue: Int){
+        sliderInput.value = newValue
+    }
+
+    fun onDropDownChanged(newValue: Int){
+        dropdownInput.value = newValue
     }
 }
