447ebe67b4bd1b3dfe0679bac048de5b3b483231
navaronbracke <brackenavaron@gmail.com>
2020-05-10 14:16:28 +0200

    Add android unit test

--- android/app/src/test/java/com/example/bachelorproef/ExampleUnitTest.kt
+++ android/app/src/test/java/com/example/bachelorproef/ExampleUnitTest.kt
@@ -1,17 +1,20 @@
 package com.example.bachelorproef
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        //Arrange
+        val c = Calculator()
+        //Act
+        val addedNumber = c.addNumbers(2,2)
+        //Assert
+        assertEquals(4, addedNumber)
     }
+}
+
+class Calculator {
+    fun addNumbers(a: Int, b: Int): Int = a+b
 }
