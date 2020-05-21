ef1cc3de2dbb2da829c5e3813f5e1692f6a6f85a
navaronbracke <brackenavaron@gmail.com>
2020-05-21 14:17:31 +0200

    Add Settings tab switch

---
+++ android/app/src/main/java/com/example/bachelorproef/ui/fragment/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.bachelorproef.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bachelorproef.R
+
+class SettingsFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar!!.title = getString(R.string.menu_settings)
+    }
+}
--- android/app/src/main/java/com/example/bachelorproef/ui/fragment/ShowcaseFragment.kt
+++ android/app/src/main/java/com/example/bachelorproef/ui/fragment/ShowcaseFragment.kt
@@ -3,7 +3,10 @@ package com.example.bachelorproef.ui.fragment
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.bachelorproef.R
+import kotlinx.android.synthetic.main.fragment_showcase.*
 
 
 class ShowcaseFragment : Fragment() {
@@ -11,5 +14,19 @@ class ShowcaseFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         return inflater.inflate(R.layout.fragment_showcase, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottomNav.setOnNavigationItemSelectedListener {
+            val ctrl = requireActivity().findNavController(R.id.showcaseNavHost)
+            if(it.itemId == R.id.bottom_nav_showcase){
+                ctrl.navigate(R.id.showcaseChoiceFragment)
+            }else{
+                ctrl.navigate(R.id.settingsFragment)
+            }
+
+            true
+        }
     }
 }
